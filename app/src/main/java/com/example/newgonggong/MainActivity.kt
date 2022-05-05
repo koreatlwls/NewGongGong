2241ebe830dadc7dfe7f8f925d7c1473acbf0ae8
koreatlwls <koreatlwls@gmail.com>
2022-05-05 10:06:10 +0900

    Project Settings Project Settings

---
+++ app/src/main/java/com/example/newgonggong/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.newgonggong
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.newgonggong.ui.theme.NewGongGongTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NewGongGongTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    NewGongGongTheme {
+        Greeting("Android")
+    }
+}
